780b31c0830ee88249ee5bc32fd786938b9759bf
Vinayak-Rajput <vk.ra.org@gmail.com>
2026-09-22 21:16:49 +0530

    feat: Initial Configuration for project

---
+++ src/main/java/com/hdfclife/ledger/PolicyLedgerApplication.java
@@ -0,0 +1,13 @@
+package com.hdfclife.ledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyLedgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PolicyLedgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hdfclife/ledger/PolicyLedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdfclife.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyLedgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
